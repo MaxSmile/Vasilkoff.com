2be31a286f6cb513dfac055ea73d9942e2e016e1
Maxim Vasilkov <dev@smrt16.com>
2023-03-23 00:18:36 +0200

    services text

--- data/services.js
+++ data/services.js
@@ -6,46 +6,41 @@ export const services = {
     "image": "/assets/images/design.png",
     "altImage": "Design from Vasilkoff Ltd",
     "title": "UI/UX Design",
-    "description": "Simplify your digital journey with our expert UI/UX design services. Our team creates designs that are both beautiful and functional, delivering a seamless and engaging user experience that captures your audience's attention.",
+    "subtitle":"Crafting Exceptional UI/UX Design: Elevating User Experience Across Platforms",
+    "description": `Simplify your digital journey with our expert UI/UX design services. 
+    Discover how our expertise in UI/UX design creates captivating user experiences for web, mobile apps, eCommerce, and beyond.
+    Great user experiences are the cornerstone of successful digital products, including decentralized applications (DApps)`,
     "slug": "design",
-    "process": {
-      "title": "Our design process",
-      "description": "Our comprehensive design strategy ensures a perfectly crafted design for your business.",
-      "strategies": [
-        {
-          "id": 1,
-          "title": "Discover",
-          "subtitle": "our four step process",
-          "description": "Donec metus lorem, vulputate at sapien sit amet, auctor iaculis lorem. In vel hendrerit nisi. Vestibulum eget risus velit. Aliquam tristique libero at dui sodales, et placerat orci lobortis. Maecenas ipsum neque, elementum id dignissim et, imperdiet vitae mauris.",
-          "highlightColor": "extra04-color",
-          "image": "/images/b1p1.jpg"
-        },
-        {
-          "id": 2,
-          "title": "Prototype",
-          "subtitle": "our four step process",
-          "description": "Donec metus lorem, vulputate at sapien sit amet, auctor iaculis lorem. In vel hendrerit nisi. Vestibulum eget risus velit. Aliquam tristique libero at dui sodales, et placerat orci lobortis. Maecenas ipsum neque, elementum id dignissim et, imperdiet vitae mauris.",
-          "highlightColor": "extra05-color",
-          "image": "/images/b1p2.jpg"
-        },
-        {
-          "id": 3,
-          "title": "Test",
-          "subtitle": "our four step process",
-          "description": "Donec metus lorem, vulputate at sapien sit amet, auctor iaculis lorem. In vel hendrerit nisi. Vestibulum eget risus velit. Aliquam tristique libero at dui sodales, et placerat orci lobortis. Maecenas ipsum neque, elementum id dignissim et, imperdiet vitae mauris.",
-          "highlightColor": "extra06-color",
-          "image": "/images/b1p3.jpg"
-        },
-        {
-          "id": 4,
-          "title": "Build",
-          "subtitle": "our four step process",
-          "description": "Donec metus lorem, vulputate at sapien sit amet, auctor iaculis lorem. In vel hendrerit nisi. Vestibulum eget risus velit. Aliquam tristique libero at dui sodales, et placerat orci lobortis. Maecenas ipsum neque, elementum id dignissim et, imperdiet vitae mauris.",
-          "highlightColor": "extra07-color",
-          "image": "/images/b1p4.jpg"
-        }
-      ]
-    }
+    "article": `
+Great user experiences are the cornerstone of successful digital products. Our team of skilled UI/UX designers is dedicated to crafting visually stunning and highly functional designs for a wide range of platforms and applications, including web, mobile apps, eCommerce, and more. We prioritize user-centric design approaches that drive user engagement, satisfaction, and ultimately, business success.
+
+## Web Design
+Our UI/UX design team creates visually appealing and user-friendly website designs that cater to diverse industries and business needs. We ensure that your website delivers an optimal user experience, encouraging visitors to explore, engage, and convert.
+
+## Mobile App Design
+We design captivating UI/UX for both native iOS and Android applications, as well as cross-platform apps developed using Flutter. Our mobile app designs prioritize usability, accessibility, and platform-specific guidelines, ensuring a seamless user experience across devices.
+
+## eCommerce Design
+Our designers specialize in creating intuitive and conversion-focused UI/UX designs for major eCommerce platforms like Magento and Shopify. We craft online stores that showcase your products effectively, streamline the shopping experience, and drive customer engagement.
+
+## DApp Design
+In the world of decentralized applications, user experience plays a crucial role in adoption and engagement. Our designers are well-versed in crafting intuitive and visually appealing UI/UX designs for DApps built on various blockchain platforms. We focus on creating seamless and secure user experiences that facilitate interactions with smart contracts, tokenization, and other decentralized features.
+
+## Business and Entertainment Apps
+We understand the unique design requirements for various types of business and entertainment apps, including fintech, utilities, simple games, on-demand services, news and media, coupon and discount, event booking, and health and fitness. Our tailored design approach ensures that each app offers an exceptional user experience that supports your business objectives.
+
+## Responsive Design
+Our UI/UX designs adapt seamlessly to various screen sizes and devices, ensuring a consistent and enjoyable user experience for your audience, regardless of how they access your digital products.
+
+## User-Centric Approach
+We prioritize a user-centric design approach, conducting extensive user research and testing to create designs that resonate with your target audience. This focus on user needs and preferences results in higher engagement, satisfaction, and conversion rates.
+
+## Continuous Improvement and Iteration
+Our design process is agile and iterative, incorporating user feedback and data-driven insights to refine and optimize the user experience over time. This approach allows us to adapt to evolving user needs and market trends, ensuring your digital products remain competitive and relevant.
+
+
+By leveraging our expertise in UI/UX design, we create captivating and user-friendly experiences across web, mobile apps, eCommerce, and various business and entertainment applications. Our user-centric approach and commitment to continuous improvement ensure that your digital products consistently delight your users and drive business success. Entrust your UI/UX design needs to our talented team and elevate your digital presence to new heights.
+    `,
   },
   "web-development":{
     "id": 2,
