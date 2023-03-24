254f6af9331ee10fe184edfe25b1ed706dfe7105
Maxim Vasilkov <dev@smrt16.com>
2023-03-24 11:30:46 +0200

    5

--- pages/index.tsx
+++ pages/index.tsx
@@ -393,11 +393,13 @@ const Index = (props: any) => {
                         <h6>Meet Our Team</h6>
                         <h4>Ingenious Problem-Solvers</h4>
                     </div>
-                    <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-4">
-                        <TeamMember name="Maxim Vasilkov" position="CBDO" picture="/images/maxim-vasilkov.png" />
+                    <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-5">
+                        
                         <TeamMember name="Simon Papazov" position="Managing Director" picture="/images/simon-papazov.png" />
                         <TeamMember name="Anastasia Sarlidou" position="Project Manager" picture="/images/anastasia-sarlidou.png" />
                         <TeamMember name="Irina Vasilkova" position="Graphics Designer" picture="/images/irina-vasilkova.png" />
+                        <TeamMember name="Anastasia Sarlidou" position="Project Manager" picture="/images/anastasia-sarlidou.png" />
+                        <TeamMember name="Maxim Vasilkov" position="CBDO" picture="/images/maxim-vasilkov.png" />
                     </div>
                 </div>
             </section>
