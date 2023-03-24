74b4355cf84cacbeedc8535916bb5544843c5394
Maxim Vasilkov <dev@smrt16.com>
2023-03-24 12:08:00 +0200

    180

--- components/ServiceCardOne.js
+++ components/ServiceCardOne.js
@@ -15,49 +15,49 @@ const ServiceCardOne = (
 
     return (
         <div data-aos="fade-up" data-aos-duration="1000">
-                            <div
-                                className="group rounded-3xl border-2 border-white bg-white p-6 transition duration-500 hover:border-secondary"
-                                style={{
-                                    boxShadow: '-20px 30px 70px rgba(219, 222, 225, 0.4)',
-                                }}
-                            >
-                                <div
-                                    className="flex h-14 w-14 items-center justify-center rounded-full bg-secondary transition group-hover:bg-black"
-                                    // style={{
-                                    //     boxShadow: '0px 15px 30px rgba(180, 118, 229, 0.4)',
-                                    // }}
-                                >
-                                    {icon}
-                                </div>
-                                <Link
-                                    href={"/services/"+slug}
-                                    className="my-8 inline-block text-[22px] font-extrabold text-black dark:text-white dark:group-hover:text-black"
-                                >
-                                    {title}
-                                </Link>
-                                <p className="mb-10 text-lg  transition line-clamp-5 group-hover:text-secondary">
-                                {message}
-                                </p>
-                                <Link
-                                    href={"/services/"+slug}
-                                    className="flex h-11 w-11 items-center justify-center rounded-full bg-[#F3F4F6] transition group-hover:bg-black rtl:rotate-180 dark:bg-gray-dark"
-                                >
-                                    <svg
-                                        width="20"
-                                        height="20"
-                                        viewBox="0 0 20 20"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="text-black transition group-hover:text-white dark:text-white"
-                                    >
-                                        <path
-                                            d="M9.41083 14.4109L10.5892 15.5892L16.1783 10.0001L10.5892 4.41089L9.41083 5.58922L12.9883 9.16672H5V10.8334H12.9883L9.41083 14.4109Z"
-                                            fill="currentColor"
-                                        />
-                                    </svg>
-                                </Link>
-                            </div>
-                        </div>
+            <div
+                className="group rounded-3xl border-2 border-white bg-white p-6 transition duration-500 hover:border-secondary"
+                style={{
+                    boxShadow: '-20px 30px 70px rgba(219, 222, 225, 0.4)',
+                }}
+            >
+                <div
+                    className="flex h-14 w-14 items-center justify-center rounded-full bg-secondary transition group-hover:bg-black"
+                // style={{
+                //     boxShadow: '0px 15px 30px rgba(180, 118, 229, 0.4)',
+                // }}
+                >
+                    {icon}
+                </div>
+                <Link
+                    href={"/services/" + slug}
+                    className="my-8 inline-block text-[22px] font-extrabold text-black dark:text-white dark:group-hover:text-black"
+                >
+                    {title}
+                </Link>
+                <p className="mb-10 text-lg  transition line-clamp-5 group-hover:text-secondary">
+                    {message}
+                </p>
+                <Link
+                    href={"/services/" + slug}
+                    className="flex h-11 w-11 items-center justify-center rounded-full bg-[#F3F4F6] transition group-hover:bg-black rtl:rotate-180 dark:bg-gray-dark"
+                >
+                    <svg
+                        width="20"
+                        height="20"
+                        viewBox="0 0 20 20"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="text-black transition group-hover:text-white dark:text-white"
+                    >
+                        <path
+                            d="M9.41083 14.4109L10.5892 15.5892L16.1783 10.0001L10.5892 4.41089L9.41083 5.58922L12.9883 9.16672H5V10.8334H12.9883L9.41083 14.4109Z"
+                            fill="currentColor"
+                        />
+                    </svg>
+                </Link>
+            </div>
+        </div>
     );
 };
 
--- data/portfolios.js
+++ data/portfolios.js
@@ -71,4 +71,231 @@ export const portfolios = [
 The Calculator Lock app is a privacy protection application for Android devices that allows users to easily hide their photos, videos, apps and files. With an inbuilt private browser, users can safely browse private websites and download photos to instantly lock them inside the photo vault. The app also includes a shake close feature for quick app closure and an intruder selfie feature that takes a picture of anyone who tries to break into the app. Additionally, users can create a fake vault with a different password for storing fake photos and videos. The app also provides fingerprint unlock for faster and more secure access. With a colourful theme and icon disguise, the app offers a variety of fashionable colours to create an exclusive theme. Users can effectively sort and organize their encrypted private files and easily locate, rename, and delete photos, pictures, and videos in the secret gallery vault. Finally, the app includes a break-in alert feature that snaps anyone who tries to break into the user's privacy.
         `,
     },
+
+
+    {
+        type:"projects",
+        categories: [
+            "Magento","eCommerce","PHP","AWS","WordPress"
+        ],
+        title: "A Better Florist",
+        link: "ABetterFlorist.com",
+        image: "https://lh3.googleusercontent.com/pw/AMWts8AZZXkCE8jrxX3vN3uqse97HFvzi6vQ4OhUkqk-153NWAvUhgs6w5ANoiVuPfq4n5TcWb4MQvl7JBbahn_MS_14sOf3jkyJwWYhkGGw9IoY_DhHn5iJod1glhZ-ktOCTDhJaEr3DIp9HIHxf4GvqJ6xCw=w2234-h1488-no?authuser=0",
+        slug: "a-better-florist",
+        url: "https://www.abetterflorist.com/",
+        extra:"",
+        description:"A Better Florist is a global network of online flower shops that offers a range of websites and a user-friendly experience for customers around the world.",
+        article:`
+A Better Florist is an ecommerce website built on the Magento platform, offering a network of online flower shops with a global presence. From www.abetterflorist.com to brisbaneblooms.com, maltafreshflowers.com to bestcakesjakarta.com, the brand offers a range of websites to cater to their customers' needs. Customers can choose from a wide range of floral arrangements, cakes, and hampers for any occasion. With easy navigation, efficient delivery services, and a range of payment options, A Better Florist aims to provide its customers with an exceptional shopping experience.
+
+brisbaneblooms.com
+goldcoastblooms.com
+adelaideflowers.co
+sydneyblooms.com
+maltafreshflowers.com
+melbourneflowers.co
+netherlandblooms.com
+montrealbloom.com
+cowtownflower.com
+bloomsvancouver.com
+flowerstoluxembourg.com
+ottawalovesflowers.com
+capetownblooms.com
+bestcakesjapan.com
+happyflowers.sg
+abetterflorist.com
+herflowers.sg
+singaporebestcakes.com
+bestcakesuk.com
+bestcakesjakarta.com
+bestcakesthailand.com
+malaysiabestcakes.com
+justperfectcakes.com
+bestcakeshk.com
+oscarscakes.com
+thailandflowers.co
+abetterfloristjapan.com
+swissflowers.co
+herflowerscz.com
+herflowersbelgium.com
+herflowersbelgium.com
+egyptblooms.com
+happyflowersmacau.com
+happyflowerschina.com
+flowersandco.co.uk
+herflowershu.com
+happyflowersmy.com
+happyflowersvietnam.com
+herflowersindo.com
+happyflowersph.com
+happyflowersjapan.com
+herflowersbangkok.com
+abetterfloristbangkok.com
+herflowers.com.hk
+abetterflorist.com.hk
+nzblooms.com
+myanmarblooms.com
+balovesblooms.com
+edinburghblooms.com
+newcastleblooms.com
+ukraineblooms.com
+lisbonlovesflowers.com
+finishflowers.com
+israeliflowers.com
+osloblooms.com
+beirutblooms.com
+romanflowers.com
+webloomcanada.com
+milanoblooms.com
+calgarycakedelivery.com
+srilankabloom.com
+happyflowers.es
+happyflowersuae.com
+happyflowerstw.com
+bloomsto.com
+abetterfloristvn.com
+abetterfloristtw.com
+abetterfloristkorea.com
+abetterflorist.com.ph
+abetterfloristpk.com
+jordanblooms.com
+qatarblooms.com
+germanblooms.com
+floralfrench.com
+abetterfloristturkey.com
+abetterfloristau.com
+canberrablooms.com
+swedishflowers.com
+perthflowers.co
+bendigoblooms.com
+ballaratblooms.com
+darwinblooms.com
+toowoombablooms.com
+hobartblooms.com
+bestcakestaiwan.com
+testserver.abetterflorist.com
+flowerstotoowoomba.com
+flowerstoballarat.com
+flowerstodarwin.com
+cairnsblooms.com
+townsvilleblooms.com
+bestcakevancouver.com
+wollongonglovesflowers.com
+bloomedmonton.com
+hongkongbesthamper.com
+almatyblooms.com
+bloomcentralcoast.com
+bestinireland.com
+mississaugaflower.com
+romanianblooms.com
+mauritiuslovesflowers.com
+polishblooms.com
+bloomswellington.com
+greekblooms.com
+saudicakes.com
+flowerstonorwich.com
+sunshinecoastflorals.com
+australiablooms.com
+perthcakedelivery.com
+capetowncakes.com
+colombocakes.com
+nottinghamblooms.com
+happyflowersfr.com
+srilankablooms.com
+
+A Better Florist is a global network of online flower shops, offering a range of websites to cater to their customers' needs. From bouquets to hampers, cakes to floral arrangements, A Better Florist provides a wide range of options for any occasion. The ecommerce website is built on the Magento platform, ensuring a user-friendly experience for customers around the world. With easy navigation, efficient delivery services, and a range of payment options, A Better Florist is committed to providing their customers with a seamless shopping experience. Whether you're in Brisbane, Malta, or Jakarta, A Better Florist has got you covered. Shop now and send your loved ones the perfect gift no matter where they are in the world.
+        `,
+    },
+    {
+        type:"projects",
+        categories: [
+            "Web3","DApp"
+        ],
+        title: "SMRT16",
+        link: "smrt16.com",
+        image: "/assets/images/cases-2.png",
+        slug: "smrt16",
+        url: "https://smrt16.com/",
+        extra:"",
+        description:"SMRT16 is an open-sourced DApp on the Polygon blockchain, offering a multi-level referral affiliate program with smart contract technology for secure and transparent tracking and rewards distribution.",
+        article:`
+        SMRT16 is an innovative DApp that offers a multi-level referral affiliate program, designed to help businesses scale their customer acquisition efforts in a cost-effective manner. The DApp is open-sourced, which means that it can be easily customized to suit individual business needs. It is published on the Polygon blockchain, which ensures fast and low-cost transactions, making it an ideal platform for businesses of all sizes. With smart contract technology, the SMRT16 DApp ensures secure and transparent referral tracking, and rewards distribution to both businesses and their affiliates. Overall, SMRT16 offers a cutting-edge solution for businesses looking to expand their customer base and revenue streams.
+        `,
+    },
+    {
+        type:"our-projects",
+        title: "SMRTSGN",
+        link: "smrtsgn.com",
+        image: "/assets/images/cases-3.png",
+        slug: "smrtsgn",
+        url: "https://smrtsgn.com/",
+        extra:"",
+        description:"SmrtSgn is a web3 app that enables secure document signing with crypto wallet signatures for transparent and tamper-proof verification.",
+        article:`
+        SmrtSgn is a cutting-edge web3 application that allows users to securely sign documents with crypto wallet signatures, enabling a transparent and tamper-proof verification process for sensitive documents.
+        `,
+    },
+    {
+        type:"projects",
+        categories: [
+            "Mobile App","iOS App","Android App","Flutter","Web3","DApp","Blockchain","Smart Contract","NFT","AI","Game"
+        ],
+        title: "Bella Chess",
+        link: "Bella Chess, Play Market, App Store",
+        image: "/assets/images/cases-4.png",
+        slug: "bellachess",
+        url: "https://bellachess.org/",
+        extra:"",
+        description:"This classic board chess game project includes a mobile app with iOS and Android support, Flutter integration, Web3, DApp, blockchain, smart contract, NFT, AI, and engaging gameplay.",
+        article:`
+        This project is a classic board chess game that offers a range of features including a mobile app, iOS and Android versions, and Flutter support. It also incorporates Web3, DApp, blockchain, and smart contract technologies to enable secure and transparent gameplay, as well as NFTs to represent unique game pieces. The game also includes AI capabilities for players to practice against and offers a highly engaging and entertaining experience for players of all skill levels.
+        `,
+    },
+    {
+        type:"projects",
+        title: "Calculator Lock",
+        link: "Play Market",
+        image: "/assets/images/cases-5.png",
+        slug: "calculator-lock",
+        url: "https://play.google.com/store/apps/details?id=com.photo.vault.calculator",
+        extra:"sm:col-span-2",
+        description:"Calculator Lock is an Android app that enables users to hide photos, videos, apps and files using a disguised calculator icon for added privacy.",
+        article:`
+The Calculator Lock app is a privacy protection application for Android devices that allows users to easily hide their photos, videos, apps and files. With an inbuilt private browser, users can safely browse private websites and download photos to instantly lock them inside the photo vault. The app also includes a shake close feature for quick app closure and an intruder selfie feature that takes a picture of anyone who tries to break into the app. Additionally, users can create a fake vault with a different password for storing fake photos and videos. The app also provides fingerprint unlock for faster and more secure access. With a colourful theme and icon disguise, the app offers a variety of fashionable colours to create an exclusive theme. Users can effectively sort and organize their encrypted private files and easily locate, rename, and delete photos, pictures, and videos in the secret gallery vault. Finally, the app includes a break-in alert feature that snaps anyone who tries to break into the user's privacy.
+        `,
+    },
+
+    {
+        type:"projects",
+        categories: [
+            "Web3","DApp"
+        ],
+        title: "NFTTRX",
+        link: "nfttrx.com",
+        image: "/assets/images/cases-1.png",
+        slug: "nfttrx",
+        url: "https://nfttrx.com/",
+        extra:"",
+        description:"NFTTRX leverages an NFT smart contract to provide transparent and secure intellectual property protection, ensuring ownership rights, copy rights, and digital content protection for creators and businesses.",
+        article:`NFTTRX is a revolutionary project that offers an NFT smart contract specifically designed for intellectual property protection. With the proliferation of digital content, it is increasingly important for creators to have a way to protect their intellectual property rights. NFTTRX provides an innovative solution by enabling creators to mint NFTs that represent their digital creations and certify ownership. The NFT smart contract ensures that ownership rights are transparent, secure and tamper-proof, making it an ideal solution for content creators and artists looking to protect their work. Overall, NFTTRX offers a cutting-edge solution that leverages blockchain technology to provide robust intellectual property protection for digital content creators.`,
+    },
+    {
+        type:"projects",
+        categories: [
+            "Web3","DApp"
+        ],
+        title: "SMRT16",
+        link: "smrt16.com",
+        image: "/assets/images/cases-2.png",
+        slug: "smrt16",
+        url: "https://smrt16.com/",
+        extra:"",
+        description:"SMRT16 is an open-sourced DApp on the Polygon blockchain, offering a multi-level referral affiliate program with smart contract technology for secure and transparent tracking and rewards distribution.",
+        article:`
+        SMRT16 is an innovative DApp that offers a multi-level referral affiliate program, designed to help businesses scale their customer acquisition efforts in a cost-effective manner. The DApp is open-sourced, which means that it can be easily customized to suit individual business needs. It is published on the Polygon blockchain, which ensures fast and low-cost transactions, making it an ideal platform for businesses of all sizes. With smart contract technology, the SMRT16 DApp ensures secure and transparent referral tracking, and rewards distribution to both businesses and their affiliates. Overall, SMRT16 offers a cutting-edge solution for businesses looking to expand their customer base and revenue streams.
+        `,
+    },
+
+ 
+
 ]
