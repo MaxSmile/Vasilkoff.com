da832500cbd70946b54fb97466c0871cfcdc381b
Maxim Vasilkov <dev@smrt16.com>
2023-04-18 20:14:08 -0400

    added Nikol

--- components/TeamMember.js
+++ components/TeamMember.js
@@ -11,7 +11,7 @@ const TeamMember = (
     return (
         <div className="group cursor-pointer text-center">
             <div className="relative h-[180px] rounded-3xl transition-all duration-500 group-hover:shadow-[0_0_25px_#979797]">
-                <img src={picture} alt="team-member-1" className="h-full w-full rounded-3xl object-cover object-top" />
+                <img src={picture} alt={name} className="h-full w-full rounded-3xl object-cover object-top" />
             </div>
             <h4 className="pt-5 pb-2 text-xl font-extrabold text-black transition duration-500 group-hover:text-secondary dark:text-white">
                 {name}
--- components/TeamMembers.js
+++ components/TeamMembers.js
@@ -9,9 +9,9 @@ const TeamMembers = (
                         
                         <TeamMember name="Simon Papazov" position="Managing Director" picture="/images/simon-papazov.png" />
                         <TeamMember name="Anastasia Sarlidou" position="Project Manager" picture="/images/anastasia-sarlidou.png" />
-                        <TeamMember name="Irina Vasilkova" position="Graphics Designer" picture="/images/irina-vasilkova.png" />
-                        <TeamMember name="Anastasia Sarlidou" position="Project Manager" picture="/images/anastasia-sarlidou.png" />
-                        <TeamMember name="Maxim Vasilkov" position="CBDO" picture="/images/maxim-vasilkov.png" />
+                        <TeamMember name="Irina Vasilkova" position="Design Director" picture="/images/irina-vasilkova.png" />
+                        <TeamMember name="Nikol Todorova" position="Graphics Designer" picture="/images/nikol.jpg" />
+                        <TeamMember name="Maxim Vasilkov" position="Systems Architect" picture="/images/maxim-vasilkov.png" />
                     </div>
     );
 };
