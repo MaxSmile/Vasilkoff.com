7eb14e449323be79fcd07ee02972c2f9a1fd7a87
Maxim Vasilkov <dev@smrt16.com>
2023-07-20 15:23:09 +0200

    minimalist ga

--- App.tsx
+++ App.tsx
@@ -2,10 +2,16 @@ import { PropsWithChildren, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { IRootState } from './store';
 import { toggleDirection, toggleTheme } from './store/themeConfigSlice';
-import ReactGA from 'react-ga4';
+//import ReactGA from 'react-ga4';
+import { track } from '@minimal-analytics/ga4';
+
 import { useRouter } from 'next/router';
 
-ReactGA.initialize("G-9WSLDWGM8P");
+// ReactGA.initialize([{
+//   "trackingId":"G-9WSLDWGM8P",
+// }]);
+
+
 
 function App({ children }: PropsWithChildren) {
     const themeConfig = useSelector((state: IRootState) => state.themeConfig);
@@ -17,9 +23,11 @@ function App({ children }: PropsWithChildren) {
 
     useEffect(() => {
         dispatch(toggleTheme(themeConfig.theme));
-        dispatch(toggleDirection(themeConfig.direction));
+        //dispatch(toggleDirection(themeConfig.direction));
+        track('G-9WSLDWGM8P');
         const handleRouteChange = (url: any) => {
-            ReactGA.send({ hitType: "pageview", page: url});
+            //ReactGA.send({ hitType: "pageview", page: url});
+            track('G-9WSLDWGM8P');
           };
       
           // When the component is mounted, subscribe to router changes
