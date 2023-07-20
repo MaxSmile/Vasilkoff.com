df9f59b0d2c954d36246e9241edd4ad5e04558be
Maxim Vasilkov <dev@smrt16.com>
2023-07-20 15:56:39 +0200

    extra for calculator lock

--- data/portfolios.js
+++ data/portfolios.js
@@ -703,7 +703,7 @@ This project is a classic board chess game that offers a range of features inclu
         image: "/assets/images/cases-5.png",
         slug: "calculator-lock",
         url: "https://play.google.com/store/apps/details?id=com.photo.vault.calculator",
-        extra:"sm:col-span-2",
+        extra:"",
         description:"Calculator Lock is an Android app that enables users to hide photos, videos, apps and files using a disguised calculator icon for added privacy.",
         article:`
 The Calculator Lock app is a privacy protection application for Android devices that allows users to easily hide their photos, videos, apps and files. With an inbuilt private browser, users can safely browse private websites and download photos to instantly lock them inside the photo vault. The app also includes a shake close feature for quick app closure and an intruder selfie feature that takes a picture of anyone who tries to break into the app. Additionally, users can create a fake vault with a different password for storing fake photos and videos. The app also provides fingerprint unlock for faster and more secure access. With a colourful theme and icon disguise, the app offers a variety of fashionable colours to create an exclusive theme. Users can effectively sort and organize their encrypted private files and easily locate, rename, and delete photos, pictures, and videos in the secret gallery vault. Finally, the app includes a break-in alert feature that snaps anyone who tries to break into the user's privacy.
