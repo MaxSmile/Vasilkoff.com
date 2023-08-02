f11431b7d2c7010d16c863fa640734da8bfcf2fb
Maxim Vasilkov <dev@smrt16.com>
2023-08-02 18:16:26 +0200

    Lazy loading for the members images

--- components/TeamMember.js
+++ components/TeamMember.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 
@@ -12,8 +13,8 @@ const TeamMember = (
 
     return (
         <div className="group cursor-pointer text-center"><Link href={"mailto:"+email}>
-            <div className="relative h-[180px] rounded-3xl transition-all duration-500 group-hover:shadow-[0_0_25px_#979797]">
-                <img src={picture} alt={name} className="h-full w-full rounded-3xl object-cover object-top" />
+            <div className="relative md:h-[180px] h-[280px] rounded-3xl transition-all duration-500 group-hover:shadow-[0_0_25px_#979797]">
+                <Image loading="lazy" width={200} height={180} src={picture} alt={name} className="h-full w-full rounded-3xl object-cover object-top" />
             </div>
             <h2 className="pt-5 pb-2 text-xl font-extrabold text-black transition duration-500 group-hover:text-secondary">
                 {name}
