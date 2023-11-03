9f2352428252db956d3184b6dd3fc4ebb8544e76
Maxim Vasilkov <dev@smrt16.com>
2023-11-03 03:13:30 +0200

    blog update

--- data/blogs.js
+++ data/blogs.js
@@ -1,4 +1,44 @@
 const blogs = {
+    "boc-hackathon-4-0":{
+      "date": "October 16, 2023",
+      "title": "BOC Fintech Hackathon 4.0: Vasilkoff's Evolution",
+      "subtitle": "Charting Vasilkoff's Progress: October 6, 2023.",
+      "description": "Vasilkoff at BOC Hackathon 4.0: Introducing innovative AI-driven live chat solutions for modern banking.",
+      "picture": "/assets/blog/boc-hackathon-4-0.webp",
+      "time": "3 min",
+      "content":`## Journey Through Hackathons
+---
+
+At [Vasilkoff](https://vasilkoff.com/), our foray into the fintech realm has always been characterized by pioneering innovation. Over the years, we have been part of various fintech hackathons, each time pushing the boundaries of what's possible and setting new benchmarks in the fintech domain.
+
+### BOC Hackathon Fintech 3.0: Making Headlines
+
+In the [BOC Hackathon Fintech 3.0](https://vasilkoff.com/blog/boc-hackathon-fintech-3-0) hosted from October 18th to 20th at the IDEA Innovation Center in Nicosia, our team showcased the 'SmartWallet', an innovative system that bridges traditional banking with electronic contracts. Represented by [Maxim Vasilkov](/team/maxim-vasilkov), [Anastasia Sarlidou](/team/anastasia-sarlidou), and [Simon Papazov](/team/simon-papazov), this innovation clinched the second prize with an award of €4,000!
+
+### VC-Armor: The Future of AI in Banking at the BOC Fintech Hackathon 4.0
+
+For the [BOC-4.0 hackathon](https://bochackathon.com/en/), we introduced VC-Armor, an avant-garde solution that harnesses the capabilities of AI to redefine modern banking experiences.
+
+#### **Key Highlights of VC-Armor:**
+
+- **Live AI Chat:** Our proprietary chatbot offers real-time interactions, meticulously designed to offer personalized insights and advice based on users' data.
+  
+- **Financial Management:** Beyond just transactions, VC-Armor delves deep into user's financial portfolios to provide bespoke financial guidance.
+
+- **Service Advertisements:** Using data-driven insights, VC-Armor recommends banking solutions perfectly aligned with the user's financial trajectory.
+
+- **Next.js Demonstration:** We've materialized VC-Armor's prowess via a [demo bank client area](https://github.com/vasilkoff/vc-armor-demo) constructed using Next.js, offering a sneak peek into its capabilities.
+
+![Screenshot](/assets/blog/Screenshot-vc-armo-chat.webp)
+
+This remarkable feat at the BOC-4.0 hackathon epitomizes Vasilkoff's persistent endeavors to spearhead fintech innovations. By interlacing AI with traditional banking, VC-Armor promises an unparalleled user experience.
+
+---
+
+For deeper insights into how AI is revolutionizing various sectors, explore our article on [AI in Business](https://vasilkoff.com/blog/ai-in-business). Dive into the transformative potential of AI and how it's making waves across different industries.
+
+`
+    },
     "ai-in-business":{
         "date": "September 7, 2023",
         "title": "AI in Business: How SmartAIChats & AI-Driven Apps Boost ROI",
