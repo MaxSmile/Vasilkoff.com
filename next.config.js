49eea0df3ab8059fd27ebc7727c5fe29307805e7
Maxim Vasilkov <dev@smrt16.com>
2023-11-13 20:21:59 +0200

    images caching

--- next.config.js
+++ next.config.js
@@ -1,14 +1,29 @@
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 });
 
 /** @type {import('next').NextConfig} */
-
-module.exports = withBundleAnalyzer({
+const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     eslint: {
         ignoreDuringBuilds: true,
-    }
-});
+    },
+    async headers() {
+        return [
+            {
+                // Match images in the public folder and its subfolders (png and webp formats)
+                source: "/:path*.(png|webp|jpg)",
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable',
+                    },
+                ],
+            },
+            // ... any other headers or configurations
+        ];
+    },
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
