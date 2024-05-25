35033563aa9bf8e6b7d00f12e96f6be0a246d356
Maxim Vasilkov <maxim.vasilkov@gmail.com>
2024-05-25 22:17:59 +0930

    v2.0.14

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -9,9 +9,9 @@ import MenuIconClose from './MenuIconClose';
 import Container from './Container';
 
 const Header = (props: any) => {
-  const [router, setRouter] = useState('');
-  const [showMenu, setShowMenu] = useState(false);
+  const [router, setRouter] = useState('')
 
+  const [showMenu, setShowMenu] = useState(false);
   const toggleMenu = () => {
     console.log("switch toggleMenu", showMenu);
     setShowMenu(!showMenu);
@@ -24,35 +24,26 @@ const Header = (props: any) => {
   return (
     <header className='sticky top-0 z-50 transition bg-white duration-300 drop-shadow-xl'>
       <Container className='h-20'>
-        <div className="flex items-center justify-between py-5 lg:py-5">
+        <div className="flex items-center justify-between py-5 lg:py-0">
           <Link href="/">
             <Image src="/images/logo-vasilkoff.webp" alt="Vasilkoff logo" className="h-12" width={104} height={48} />
           </Link>
           <div className="flex items-center">
             {/* Overlay */}
-            <div
-              id='overlay'
-              onClick={toggleMenu}
-              className={`fixed inset-0 h-screen w-full z-50 bg-black/60 ${showMenu ? '' : 'hidden'}`}
-            ></div>
+            <div id='overlay' onClick={() => toggleMenu()}
+              className={`overlay fixed inset-0 h-screen w-full z-[51] bg-black/60 ${showMenu ? '' : 'hidden'}`}></div>
 
             {/* Menu */}
-            <div
-              id='menu'
-              className={`fixed top-0 right-0 w-64 bg-white pt-1 transform transition-transform duration-300 ease-in-out lg:static lg:w-auto lg:transform-none lg:translate-x-0 lg:flex lg:items-center ${showMenu ? 'translate-x-0' : 'translate-x-full'}`}
-            >
-              <button
-                id='close-menu'
-                onClick={toggleMenu}
-                type="button"
-                aria-label="Close Menu"
-                className="lg:hidden absolute top-5 right-5 p-2 z-50 rounded-full bg-primary items-center justify-center"
-              >
+            {showMenu && <div id='menu' className={`menus top-0 pt-1 ${showMenu ? 'overflow-y-auto !right-0' : ''}`}>
+              <button id='close-menu'
+                onClick={() => toggleMenu()} type="button" aria-label="Menu"
+                className="lg:hidden absolute top-5 right-5 p-2 z-[51] rounded-full bg-primary items-center justify-center"
+                role='button'>
                 <MenuIconClose />
               </button>
-              <ul role='menu' className='flex flex-col lg:flex-row' onClick={() => setShowMenu(false)}>
+              <ul role='menu' onClick={() => setShowMenu(false)} >
                 {MAIN_MENU.map((item, index) => (
-                  <li role='menuitem' key={index} className='lg:mx-2'>
+                  <li role='menuitem' key={index}>
                     <Link href={item.link} className={router.includes(item.link) ? 'active' : ''}>
                       {item.title}
                     </Link>
@@ -60,14 +51,13 @@ const Header = (props: any) => {
                 ))}
               </ul>
             </div>
-
+            }
             <button
               id='menu-toggle'
-              type="button"
-              aria-label="Open Menu"
+              type="button" aria-label="Menu"
+              role='button'
               className="flex h-10 w-10 items-center justify-center rounded-full bg-primary lg:hidden"
-              onClick={toggleMenu}
-            >
+              onClick={() => toggleMenu()} >
               <MenuIconOpen />
             </button>
           </div>
