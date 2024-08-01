ad1d353d5e707e5f8d2d2b355478d841e2a34e35
Maxim Vasilkov <maxim.vasilkov@gmail.com>
2024-08-01 21:37:56 +0930

    metadataBase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,22 @@ export const metadata: Metadata = {
   openGraph: {
     images: [],
   },
+  creator: 'Maksym Vasylkov',
+  publisher: 'Vasilkoff Ltd',
+  metadataBase: new URL('https://vasilkoff.com'),
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  }
 };
 
 export default function RootLayout({
